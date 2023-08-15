bcc75bd36df569e7142fc557e8543ae47525b894
Azamat32 <azamat.chimchkov@gmail.com>
2023-08-15 11:38:10 +0600

    add Navbar

---
+++ client/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const MainPage = (_props: Props) => {
+  return (
+    <div>MainPage</div>
+  )
+}
+
+export default MainPage
---
+++ client/src/widgets/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+
+import "./Navbar.scss"
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="navbar">Navbar</div>
+  )
+}
+
+export default Navbar
