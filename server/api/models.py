02d0cab97ffe58464c4a4c975e5ef55c128b52b5
Musakhan <mzhumabay03@gmail.com>
2023-08-19 20:06:44 +0600

    some models

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'regions'
+        managed = False
+
+    def __str__(self):
+        return self.name
+    
+##############################################################################
+class GDP(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)  # NUMERIC в PostgreSQL
+    period = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    updated_at = models.DateField()
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'gdp'
+        managed = False
+
+########################################################################
+class LaborProductivityActivityType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'labor_productivity_activity_types'
+        managed = False
+
+    def __str__(self):
+        return self.name
+
+
+class LaborProductivity(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateField()
+    value = models.BigIntegerField()
+    period = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    updated_at = models.DateField()
+    economic_activity = models.ForeignKey(LaborProductivityActivityType, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'labor_productivity'
+        managed = False
+    
+    #######################################################
