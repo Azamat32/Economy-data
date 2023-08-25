17c1c761e1f8591b3c6c83e67e2a7e8e12fad2ae
Azamat32 <azamat.chimchikov@gmail.com>
2023-08-25 12:47:35 +0600

    some changes with models

--- server/api/models.py
+++ server/api/models.py
@@ -1,66 +1,9 @@
 from django.db import models
 
-class Region(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-
-    class Meta:
-        db_table = 'regions'
-        managed = False
-
-    def __str__(self):
-        return self.name
     
-##############################################################################
-class GDP(models.Model):
-    id = models.AutoField(primary_key=True)
-    created_at = models.DateField()
-    value = models.BigIntegerField()  # NUMERIC в PostgreSQL
-    period = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    updated_at = models.DateField()
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'gdp'
-        managed = False
-
-########################################################################
-class LaborProductivityActivityType(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-
-    class Meta:
-        db_table = 'labor_productivity_activity_types'
-        managed = False
-
-    def __str__(self):
-        return self.name
-
-
-class LaborProductivity(models.Model):
-    id = models.AutoField(primary_key=True)
-    created_at = models.DateField()
-    value = models.BigIntegerField()
-    period = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    updated_at = models.DateField()
-    economic_activity = models.ForeignKey(LaborProductivityActivityType, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'labor_productivity'
-        managed = False
-    
-    
-
 class Topic(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, unique=True)
-
-    class Meta:
-        db_table = 'topics'
-        managed = False
 
     def __str__(self):
         return self.name
@@ -70,10 +13,6 @@ class EconomicIndex(models.Model):
     name = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
     macro = models.ForeignKey(Topic, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'economic_indices'
-        managed = False
 
     def __str__(self):
         return self.name
