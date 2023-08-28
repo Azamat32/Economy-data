5b4e846ff10690857d0765ed201662bc46680476
Azamat32 <azamat.chimchkov@gmail.com>
2023-08-28 14:51:39 +0600

    add

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,42 +1,49 @@
 import { useEffect, useState } from "react";
 import EconomyItem from "../../widgets/EconomyItem/EconomyItem";
+import axios from "axios";
 import "./MainPage.scss";
 type Props = {};
-type Item = {
-  macro: {
-    id: number;
-    name: string;
-  };
+type TableTitles = {
+  id: number;
   name: string;
-  link: string;
 };
-
+const api = "economic_indices"
 const MainPage = (_props: Props) => {
-  const [items, setItems] = useState<Item[]>([]);
-
+  const [tablesTitle, setTablesTitle] = useState<TableTitles[]>([]);
+  const [tableItem , setTableItem] = useState()
   useEffect(() => {
     fetchData();
   }, []);
+  const handleClick = async (id: number) => {
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:8000/api/${api}`,
+        { id } // Sending the id in the request payload
+      );
+      setTableItem(response.data); // Assuming the response data should be saved
+      console.log(tableItem);
+      
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
 
   const fetchData = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api");
+      const response = await fetch("http://127.0.0.1:8000/api/topics");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      setItems(data); // Assuming the API returns an array of items
+      setTablesTitle(data);
+      
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-  console.log(items);
-  let tables = items.map((item) => {
+  let tables = tablesTitle.map((item) => {
     return (
-      <>
-        <div className="title">{item.macro.name} : </div>
-        <div className="text">{item.name}</div>
-      </>
+        <div onClick={() => handleClick(item.id)} className="title" key={item.id}>{item.name} </div>
     );
   });
   return (
