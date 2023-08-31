15dd26ab42b05e9974b60fae6e40b549c9ca8cc8
Azamat32 <azamat.chimchikov@gmail.com>
2023-08-31 10:35:40 +0600

    add

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -14,9 +14,7 @@ const MainPage = (_props: Props) => {
   useEffect(() => {
     fetchData();
   }, []);
-<<<<<<< HEAD
   
-=======
   const handleClick = async (id: number) => {
     try {
       const response = await axios.post(
@@ -31,7 +29,6 @@ const MainPage = (_props: Props) => {
     }
   };
 
->>>>>>> 3448d9d8cefafab64621714ef089faea8e050537
   const fetchData = async () => {
     try {
       const response = await fetch("http://127.0.0.1:8000/api/topics");
@@ -45,18 +42,7 @@ const MainPage = (_props: Props) => {
       console.error("Error fetching data:", error);
     }
   };
-<<<<<<< HEAD
-  console.log(items);
-  let products = items.map((item)=> {
-    return item.link + item.title
-  })
-  return (
-    <div className="main">
-      <div className="container">
-        <div className="main_inner">
-          {products}
-        </div>
-=======
+
   let tables = tablesTitle.map((item) => {
     return (
         <div onClick={() => handleClick(item.id)} className="title" key={item.id}>{item.name} </div>
@@ -66,7 +52,6 @@ const MainPage = (_props: Props) => {
     <div className="main">
       <div className="container">
         <div className="main_inner">{tables}</div>
->>>>>>> 3448d9d8cefafab64621714ef089faea8e050537
       </div>
     </div>
   );
