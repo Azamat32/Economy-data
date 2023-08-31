82dc571c877c7d0ff1c06a399f6932759f087a73
Azamat32 <azamat.chimchikov@gmail.com>
2023-08-31 10:33:34 +0600

    add

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -15,7 +15,7 @@ const MainPage = (_props: Props) => {
   useEffect(() => {
     fetchData();
   }, []);
-
+  
   const fetchData = async () => {
     try {
       const response = await fetch("http://127.0.0.1:8000/api");
@@ -29,11 +29,14 @@ const MainPage = (_props: Props) => {
     }
   };
   console.log(items);
-  
+  let products = items.map((item)=> {
+    return item.link + item.title
+  })
   return (
     <div className="main">
       <div className="container">
         <div className="main_inner">
+          {products}
         </div>
       </div>
     </div>
