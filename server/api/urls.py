df939f47d49b6527bc0512e14f914e721b4c5339
Musakhan <mzhumabay03@gmail.com>
2023-09-11 18:08:34 +0600

    fix bugs

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('topics', views.get_topics, name = "topics"), 
     path('economic_indices', views.get_economic_indices, name = "economic_indices"),
     path('economic_index', views.get_economic_index, name = "economic_index"),    
-    path('economic_index/excel', views.get_excel, name = "economic_index"),    
-
 ]
