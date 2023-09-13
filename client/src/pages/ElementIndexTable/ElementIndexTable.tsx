642f6750294fb01fdfe6ecf8b3bc1bd5e193eedc
Azamat32 <azamat.chimchikov@gmail.com>
2023-09-13 14:38:28 +0600

    add parse of excel file

--- client/src/pages/ElementIndexTable/ElementIndexTable.tsx
+++ client/src/pages/ElementIndexTable/ElementIndexTable.tsx
@@ -3,7 +3,8 @@ import {useEffect , useState} from "react"
 import axios from "axios";
 import { useQuery } from "react-query";
 import "./ElementIndexTable.scss";
-import ReactExcelRender from "../../widgets/ReactExcelRenderer/ReactExcelRender";
+import * as  XLSX from "xlsx"; // Import the xlsx library
+
 type RouteParams = {
   id: string; // Define the route parameter as a string
 };
@@ -34,22 +35,54 @@ const ElementIndexTable = (_props: Props) => {
     fetchElementById(id)
   );
 
-  const [excelData, setExcelData] = useState<string | null>(null); // Provide null as the initial state
+  const [excelData, setExcelData] = useState<Blob | null>(null); // Provide null as the initial state
   const [sheetData, setSheetData] = useState<any[][] | null>(null); // State to store the Excel sheet data
 
   useEffect(() => {
-    // Fetch the Excel file when the component mounts
     fetchExcelFile()
       .then((excelFile) => {
         // Create a blob URL for the Excel file
-        const blob = new Blob([excelFile], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
-        const url = window.URL.createObjectURL(blob);
-        setExcelData(url);
+        const blob = new Blob([excelFile], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+  
+        setExcelData(blob);
+  
+        // Parse the Excel file
+        parseExcelFile(blob);
       })
       .catch((error) => {
         console.error("Error fetching Excel file:", error);
       });
   }, [id]);
+  
+
+  const parseExcelFile = (file: Blob) => {
+    const reader = new FileReader();
+  
+    reader.onload = (e) => {
+      const data = (e.target as FileReader).result;
+
+      const workbook = XLSX.read(data, { type: "binary" });
+
+      
+      // Assuming you want the data from the first sheet
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+  
+      // Parse the sheet data into an array of objects
+      const excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  
+      // Set the parsed data in state
+      setSheetData(excelData as any[][]);
+
+    };
+  
+    reader.readAsBinaryString(file);
+  };
+  
+
+
 
 
   if (isLoading) {
@@ -63,11 +96,35 @@ const ElementIndexTable = (_props: Props) => {
     <div>
       <div className="table_title">
         <h2> {data.name}</h2>
-        {excelData && (
+        {/* {excelData && (
           <a href={excelData} download="test.xlsx">
             Download Excel File
           </a>
-        )}
+        )} */}
+
+{sheetData && (
+      <div>
+        <h3>Excel Data</h3>
+        <table>
+          <thead>
+            <tr>
+              {sheetData[0].map((header: string, index: number) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {sheetData.slice(1).map((row: any[], rowIndex: number) => (
+              <tr key={rowIndex}>
+                {row.map((cell: any, cellIndex: number) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )}
       </div>
     </div>
   );
