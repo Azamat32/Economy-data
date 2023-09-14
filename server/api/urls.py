fe1c725014aa42db5e9fe40726bef3fc0598a7aa
Musakhan <mzhumabay03@gmail.com>
2023-09-14 21:44:00 +0600

    added tables

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('topics', views.get_topics, name = "topics"), 
     path('economic_indices', views.get_economic_indices, name = "economic_indices"),
     path('economic_index', views.get_economic_index, name = "economic_index"),    
-    path('economic_index/excel', views.get_excel, name = "economic_index"),    
+    path('economic_index_excel', views.get_economic_index_excel, name = "economic_index"),    
 
 ]
