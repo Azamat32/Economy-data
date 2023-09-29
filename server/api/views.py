b2a1bedbf74f65b8e6d986e9e19b6d64562933c3
Musakhan <mzhumabay03@gmail.com>
2023-09-29 14:22:49 +0600

    new view

--- server/api/views.py
+++ server/api/views.py
@@ -1,12 +1,12 @@
-#from rest_framework.views import APIView
+from rest_framework.views import APIView
 from rest_framework import generics
-from .models import Economic_index, Topic
+from .models import Economic_index, Topic, Table
 from .serializer import EconomicIndexSerializer, TopicSerializer
-#from django.http import FileResponse, JsonResponse
+from django.http import FileResponse, JsonResponse
 from django.http import Http404
 #from rest_framework.response import Response
 #from rest_framework import status
-#import os
+import os
 
 class GetTopics(generics.ListAPIView):
     queryset = Topic.objects.all()  
@@ -30,18 +30,18 @@ class GetEconomicIndex(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EconomicIndexSerializer
 
 
-# class GetEconomicIndexExcel(APIView):
-#     def get(self, request, pk):
-#         try:
-#             tables = Table.objects.filter(macro_economic_index__id=pk)
-#             excel_file_path = os.path.join('static', 'Excel', tables.path)  
-#             if os.path.isfile(excel_file_path):
-#                 excel_response = FileResponse(open(excel_file_path, 'rb'), as_attachment=True, filename=related_indices.path)
-#                 return excel_response
-#             else:
-#                 return JsonResponse({"error": "Excel file not found."}, status=404)
-#         except Economic_index.DoesNotExist:
-#             return JsonResponse({"error": "Economic index with the specified ID does not exist."}, status=404) 
+class GetEconomicIndexExcel(APIView):
+     def get(self, request, pk):
+         try:
+             tables = Table.objects.get(id=pk)
+             excel_file_path = os.path.join('static', 'tables', tables.path + '.xlsx')  
+             if os.path.isfile(excel_file_path):
+                 excel_response = FileResponse(open(excel_file_path, 'rb'), as_attachment=True, filename=tables.path)
+                 return excel_response
+             else:
+                 return JsonResponse({"error": "Excel file not found."}, status=404)
+         except Economic_index.DoesNotExist:
+             return JsonResponse({"error": "Economic index with the specified ID does not exist."}, status=404) 
 
 # class SaveExcel(APIView):
 #     def post(self, request, pk):
